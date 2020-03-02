cd596efa83e3f2835c9379d7395606d9558c7c6a
syjNB001 <61702971+syjNB001@users.noreply.github.com>
2020-03-03 00:49:06 +0800

    Update new_file.js

--- new_file.js
+++ new_file.js
@@ -1,1 +1,26 @@
-
+(function (){
+	var oleft = document.querySelector('.left');
+	var ol1 = document.querySelector('.l1');
+	var ol2 = document.querySelector('.l2');
+	var ocon = document.querySelector('.con');
+	var oright = document.querySelector('.right');
+	index = 0;//图片第几张的索引
+	oclock = false;
+	function fanye (){
+		if(oclock) return ;
+		oclock = true ;
+		index++;
+		index%=7;
+		oleft.classList.add('on');//添加类名
+		document.addEventListener('transitionend',function(){
+			oleft.classList.remove('on');
+			ol1.style.backgroundImage = ocon.style.backgroundImage = 'url("jpg/vee'+index+'.png")';
+			ol2.style.backgroundImage = oright.style.backgroundImage = 'url("jpg/vee'+(index+1)%7+'.png")';
+			oclock = false;
+				});//事件监听，翻页结束执行
+		}
+		// document.onclick = function (){
+		// 	fanye();
+		// }
+		setInterval(fanye,1500);
+})();
